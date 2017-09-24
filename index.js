59c81992c2612180b86214df41172d53b4af495d
Alexandra Capellini <acapell1@jhu.edu>
2017-09-24 18:02:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,22 +2,14 @@ function shout(string) {
   return string.toUpperCase()
 };
 
-shout("hello");
-
 function whisper(string) {
   return string.toLowerCase ()
 };
-
-whisper("HELLO");
 
 function logShout(string) {
   return console.log(string.toUpperCase)
 };
 
-logShout("hello");
-
 function logShout (string) {
   return console.log(string.toLowerCase)
-};
-
-logShout("")
+}
